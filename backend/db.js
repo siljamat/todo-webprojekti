e493c4b7f7fd0d1a60a035af8ffc78e63095acba
tuiskuna <tuiskunarhi@gmail.com>
2023-04-14 08:50:33 +0300

    kommentoitua koodia

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,19 @@
-const mongoose = require("mongoose")
+// Importing the Mongoose module
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/Todoapp");
+// Storing the connection string for the MongoDB Atlas cluster in a variable called uri
+const uri = 'mongodb+srv://tuisku:mongoPass@todocluster.mner2t0.mongodb.net/?retryWrites=true&w=majority';
 
+// Connecting to the MongoDB Atlas cluster using the Mongoose connect method
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        // Logging a message to the console if the connection is successful
+        console.log('Successfully connected to MongoDB');
+    })
+    .catch((error) => {
+        // Logging an error message to the console if the connection is unsuccessful
+        console.log('Error connecting to MongoDB', error);
+    });
 
-module.exports = mongoose
+// Exporting the Mongoose connection object to be used in other parts of the application
+module.exports = mongoose.connection;
--- todoapp/src/Components/Date.jsx
+++ todoapp/src/Components/Date.jsx
@@ -1,30 +1,26 @@
-import React from 'react';
-import { useState } from 'react';
+import React from 'react'; // Importing React library to use React components
+import { useState } from 'react'; // Importing the useState hook from the React library
 
 const D = () => {
 
-    const[Ctime , setCtime] = useState()
+    const[Ctime , setCtime] = useState() // Declaring and initializing a state variable "Ctime" and its setter function "setCtime"
 
+    let date = new Date().toLocaleDateString() // Initializing a "date" variable with the current date in string format
 
-    let date = new Date().toLocaleDateString()
-   
+    const tTime = () => { // Defining a function "tTime"
 
-   const tTime = () => {
+        let time = new Date().toLocaleTimeString(); // Initializing a "time" variable with the current time in string format
+        setCtime(time); // Updating the state variable "Ctime" with the value of "time"
 
-  
-    let time = new Date().toLocaleTimeString();
-    setCtime(time);
-   
+    }
 
-   }
-   setInterval(tTime , 1000);
+    setInterval(tTime , 1000); // Setting up a timer to call the "tTime" function every 1 second
 
     return (
         <>
-            <p>{date}  {Ctime}</p>
-
+            <p>{date}  {Ctime}</p> // Displaying the current date and time stored in the state variable "Ctime"
         </>
     );
 };
 
-export default D;
+export default D; // Exporting the component "D" as the default export of this module
