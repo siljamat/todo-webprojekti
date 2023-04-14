b8282c2fb0fe5f7e93b9b009290bb9ee0ec01a3f
tuiskuna <tuiskunarhi@gmail.com>
2023-04-14 08:52:22 +0300

    kommentoitua koodia

--- todoapp/src/Home.jsx
+++ todoapp/src/Home.jsx
@@ -6,28 +6,34 @@ import Task from './Components/todolist/Task';
 import Date from './Components/Date';
 import './Home.css'
 
-
-
 const Home = () => {
 
-  const navigate = useNavigate()
- 
+    const navigate = useNavigate()
 
+    // Render the Home component, which includes a navigation bar,
+    // a clock display and a list of tasks
     return (
         <>
-        <div className='backg'>
-             <div className='navbar'>
-                <button type='button' className='navbtn' onClick={() => navigate("/categories")}><Icon.List size={30}/></button>
-                <h2>Schedule</h2>
-                <Addtask/>
-            </div>
+            <div className='backg'>
+                {/* The navigation bar */}
+                <div className='navbar'>
+                    {/* Button to navigate to categories */}
+                    <button type='button' className='navbtn' onClick={() => navigate("/categories")}>
+                        <Icon.List size={30}/>
+                    </button>
+                    <h2>Schedule</h2>
+                    {/* Addtask component to add a new task */}
+                    <Addtask/>
+                </div>
+                {/* The clock display */}
                 <div className='clock'>
                     <Date/>
                 </div>
-            <div>
-                <Task/>
+                {/* The list of tasks */}
+                <div>
+                    <Task/>
+                </div>
             </div>
-        </div>
         </>
     );
 };
