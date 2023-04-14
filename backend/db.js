debe2f3df3425ced2e5000e505c9bfd2d8e9f78c
siljamat <siljama@hotmail.fi>
2023-04-14 13:03:46 +0300

    database yhteys

--- backend/db.js
+++ backend/db.js
@@ -1,19 +1,14 @@
-// Importing the Mongoose module
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
-// Storing the connection string for the MongoDB Atlas cluster in a variable called uri
-const uri = 'mongodb+srv://tuisku:mongoPass@todocluster.mner2t0.mongodb.net/?retryWrites=true&w=majority';
+const user = "siljama"
+const password = "salasana12345"
+const clusterUrl = "todo.pfpdhkb.mongodb.net"
+const dbName = "Todo"
 
-// Connecting to the MongoDB Atlas cluster using the Mongoose connect method
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        // Logging a message to the console if the connection is successful
-        console.log('Successfully connected to MongoDB');
-    })
-    .catch((error) => {
-        // Logging an error message to the console if the connection is unsuccessful
-        console.log('Error connecting to MongoDB', error);
-    });
+const uri = `mongodb+srv://${user}:${password}@${clusterUrl}/${dbName}?retryWrites=true&w=majority`
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
-// Exporting the Mongoose connection object to be used in other parts of the application
-module.exports = mongoose.connection;
+module.exports = mongoose
