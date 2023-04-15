39aa6a7525f6ca3d29d529c9e4719a2f99c4579a
sanna <sanna.lohkovuori@hotmail.com>
2023-04-15 20:08:15 +0300

    Todot mahdollista katsoa päiväkohtaisesti ja kuukausikohtaisesti

--- todoapp/src/Components/todolist/Task.jsx
+++ todoapp/src/Components/todolist/Task.jsx
@@ -1,95 +1,128 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as Icon from 'react-bootstrap-icons'
 import { useNavigate } from 'react-router-dom'
 import './Task.css'
-import { useEffect , useState } from 'react';
 import axios from 'axios';
 import DatePicker from 'react-date-picker';
 
 
 const Task = () => {
 
-    const[todolist , settodolist] = useState([]) // Initializing state for the list of tasks
-    const navigate = useNavigate() // Initializing the navigation hook for use in the component
+    const [todolist, setTodolist] = useState([]); // Initializing state for the list of tasks
+    const [value, onChange] = useState(new Date()); // Initializing state for the selected date in the date picker component
+    const [showAll, setShowAll] = useState(false); // Initializing state to toggle between showing todos for the selected day and the entire month
+    const navigate = useNavigate(); // Initializing the navigation hook for use in the component
 
-    //------------- Get all Todolist from datatbase ---------------///
-    useEffect(() =>{
+    //------------- Get all Todolist from database ---------------///
+    useEffect(() => {
         axios.get("http://localhost:2000/todolist") // Making a GET request to fetch the list of tasks from the database
             .then(async (res) => {
-                const rawtodolist = await res.data; // Extracting the data from the response object
-                settodolist(rawtodolist); // Updating the state with the list of tasks fetched from the database
-            }).catch((err) =>{console.log(err)}) // Handling errors if any
-    })
+                const rawTodolist = await res.data; // Extracting the data from the response object
+                setTodolist(rawTodolist); // Updating the state with the list of tasks fetched from the database
+            })
+            .catch((err) => console.log(err)); // Handling errors if any
+    }, []);
 
-    ///----------- Filter todolist via Date -----------///
-    const [value, onChange] = useState(new Date()); // Initializing state for the selected date in the date picker component
-    let seldate = value.toLocaleDateString() // Converting the selected date to a string in the format 'mm/dd/yyyy'
+    // Filtering the list of tasks based on the selected date or the entire month
+    let filterlist = todolist.filter((row) => {
+        const newdate = row.date;
+        const d = new Date(newdate);
+        const selMonth = value.getMonth();
+        const rowMonth = d.getMonth();
 
-    // Filtering the list of tasks based on the selected date
-    let filterlist = todolist.filter( row => {
-        let newdate = row.date // Extracting the date for each task
-        let d = new Date(newdate) // Converting the date string to a date object
-        let da= d.toLocaleDateString() // Converting the date object to a string in the format 'mm/dd/yyyy'
-
-        // If the date for the task is not valid, return the task (this handles tasks with missing dates)
-        if (da == "null") {
-            return (row)
+        if (showAll) {
+            // Show all todos for the selected month
+            return rowMonth === selMonth;
+        } else {
+            // Show todos for the selected day only
+            const selDate = value.toLocaleDateString(); // Converting the selected date to a string in the format 'mm/dd/yyyy'
+            const rowDate = d.toLocaleDateString(); // Converting the date object to a string in the format 'mm/dd/yyyy'
+            return selDate === rowDate;
         }
-        else {
-            // If the date for the task matches the selected date, return the task
-            return (da == seldate)
-        }
-    })
+    });
 
-    //-------------- Delete Handler -----------//
-    const deleteHandler = async(_id) => {
-        await axios.delete(`http://localhost:2000/todolist/${_id}`) // Making a DELETE request to delete the selected task from the database
+    // Delete handler to delete a task from the list
+    const deleteHandler = async (_id) => {
+        await axios.delete(`http://localhost:2000/todolist/${_id}`); // Making a DELETE request to delete the selected task from the database
         alert("Task Deleted Successfully"); // Displaying an alert to inform the user that the task has been deleted
-        window.location.reload() // Reloading the page to update the list of tasks displayed
-    }
+        window.location.reload(); // Reloading the page to update the list of tasks displayed
+    };
+
+    // Toggles the showAll state to show todos for the entire month
+    const handleShowAll = () => {
+        setShowAll(true);
+    };
+
+    // Toggles the showAll state to show todos for the selected day
+    const handleShowDay = () => {
+        setShowAll(false);
+    };
 
     // Rendering the component
     return (
         <>
             <div className='fil'>
-                <DatePicker  onChange={onChange} value={value} /> // Displaying the date picker component
+                <DatePicker onChange={onChange} value={value} /> // Displaying the date picker component
+                <button onClick={handleShowAll}>Show all todos for this month</button>
+                <button onClick={handleShowDay}>Show todos for selected day only</button>
             </div>
 
-            {
-                // Mapping through the filtered list of tasks and rendering each task as a card
-                filterlist.map((row) => {
-                    const newdate = row.date
-                    const d = new Date(newdate);
+            {filterlist.map((row) => {
+                const newdate = row.date;
+                const d = new Date(newdate);
 
-                return(
+                return (
                     <>
-                     <div className='catecont'>
-                        <div className='cateRow'>
-                        <div className=''>
-                            <div className='time'>
-                                <p>{d.getHours()}:{d.getMinutes()}</p>
+                        <div className='catecont'>
+                            <div className='cateRow'>
+                                <div className=''>
+                                    <div className='time'>
+                                        <p>{d.getHours()}:{d.getMinutes()}</p>
+                                    </div>
+                                </div>
+
+                                <div className='nme'>
+                                    <h3>{row.taskName.toUpperCase()}</h3>
+                                    <p>{row.category}</p>
+                                </div>
+
+                                <div class="dropdown">
+                                    <button
+                                        class="btn"
+                                        type="button"
+                                        data-bs-toggle="dropdown"
+                                        aria-expanded="false"
+                                    >
+                                        <Icon.ThreeDotsVertical />
+                                    </button>
+                                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                        <li>
+                                            <button
+                                                type='button'
+                                                className='navbtn dropdown-item '
+                                                onClick={() => navigate(`/edittask/${row._id}`)}
+                                            >
+                                                <Icon.PencilSquare />
+                                                Edit
+                                            </button>
+                                        </li>
+                                        <li>
+                                            <button
+                                                type='button'
+                                                className='navbtn dropdown-item'
+                                                onClick={() => deleteHandler(row._id)}
+                                            >
+                                                <Icon.XLg />
+                                                Delete
+                                            </button>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-
-
-                       <div className='nme'>
-                            <h3>{row.taskName.toUpperCase()}</h3>
-                            <p>{row.category}</p>
-                       </div>
-                       
-                        <div class="dropdown">
-                                        <button class="btn" type="button" data-bs-toggle="dropdown" aria-expanded="false"><Icon.ThreeDotsVertical/></button>
-                                            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                                <li><button type='button' className='navbtn dropdown-item ' onClick={() => navigate(`/edittask/${row._id}`)}><Icon.PencilSquare/> Edit</button></li>
-                                                <li><button type='button' className='navbtn dropdown-item' onClick={() => deleteHandler(row._id)}><Icon.XLg/> Delete</button></li>
-                                            </ul>
-                         </div>
-                    </div> 
-                    </div>  
                     </>
-                )
-             })
-          }
+                );
+            })}
         </>
     );
 };
